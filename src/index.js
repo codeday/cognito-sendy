9301ec2a6e6f2a347e4f7e4380c6ff5d6b3f6162
Tyler Menezes <github@xanadu.ws>
2020-05-11 17:25:31 -0400

    Add healthcheck

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const app = require('express')();
 const bodyParser = require('body-parser');
 const { dereferenceDottedString } = require('./utils');
@@ -6,13 +7,15 @@ const subscribe = require('./sendy');
 const port = process.env.PORT || 80;
 app.use(bodyParser.json());
 
+app.get('/health', (_, res) => res.send('ok'));
+
 app.post('/', (req, res) => {
   const { list, ...query } = req.query;
 
   const sendyData = Object.keys(query)
     .reduce((accum, k) => ({
       ...accum,
-      [k]: dereferenceDottedString(query[k], req.body)
+      [k]: dereferenceDottedString(query[k], req.body),
     }), {});
 
   try {
@@ -27,4 +30,4 @@ app.post('/', (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Listening at http://0.0.0.0:${port}/`))
+app.listen(port, () => console.log(`Listening at http://0.0.0.0:${port}/`));
